1f5680b9ece49068f0e481b7a6e11ab8970661e4
Katrina Rosales <katrinarosales@live.com>
2016-11-28 16:24:04 +0100

    Test

---
+++ Adressbok/Controllers/AdressController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Adressbok.Models;
+
+namespace Adressbok.Controllers
+{
+    public class AdressController : Controller
+    {
+        public static IList<Adressboken> AdressList = new List<Adressboken>();
+
+        // GET: Adress
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Adressboken newAdress)
+        {
+            AdressList.Add(newAdress);
+            return PartialView("ListOfAdress", AdressList);
+        }
+
+        public ActionResult ListOfAdress()
+        { 
+            return PartialView("ListOfAdress", AdressList);
+        }
+
+        public ActionResult Edit(int id, string namn, string adress)
+        {
+            var showAdress = AdressList.FirstOrDefault(x => x.Id == id);
+            return View(showAdress);
+        }
+        [HttpPost]
+        public ActionResult Edit(int adressID)
+        {
+            var updatedAdress = AdressList.First().Id == adressID;
+            return View(updatedAdress);
+        }
+    }
+}
---
+++ Adressbok/Models/Adressboken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adressbok.Models
+{
+    public class Adressboken
+    {
+        public int Id { get; set; }
+        public string Namn { get; set; }
+        public int Telefonnummer { get; set; }
+        public string Adress { get; set; }
+
+    }
+}
